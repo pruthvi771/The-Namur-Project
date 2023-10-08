c55a1d50eba0b382c49c619cd7de57a54baab2a5
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2023-10-09 02:06:15 +0530

    Fully functioning dropdown for District and Taluks along with validation. Corrected Validation for Land addition.

--- lib/features/profile/screens/edit_profile.dart
+++ lib/features/profile/screens/edit_profile.dart
@@ -8,21 +8,13 @@ import 'package:active_ecommerce_flutter/features/profile/hive_bloc/hive_state.d
 import 'package:active_ecommerce_flutter/features/profile/hive_models/models.dart';
 import 'package:active_ecommerce_flutter/features/profile/screens/more_details.dart';
 import 'package:flutter/material.dart';
-
-import 'package:active_ecommerce_flutter/features/profile/expanded_tile_widget.dart';
-import 'package:active_ecommerce_flutter/features/profile/screens/land_screen.dart';
 import 'package:active_ecommerce_flutter/my_theme.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:hive/hive.dart';
 import 'package:toast/toast.dart';
-import '../../../custom/device_info.dart';
 
-import 'package:flutter_expanded_tile/flutter_expanded_tile.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:active_ecommerce_flutter/features/profile/address_list.dart'
+    as addressList;
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
@@ -32,14 +24,10 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
-  final String title = 'KYC';
-  var _textController = TextEditingController();
-  String hintText = "Email ID";
-
   // TextEditingController _districtController = TextEditingController();
   // TextEditingController _talukController = TextEditingController();
-  // TextEditingController _hobliController = TextEditingController();
-  // TextEditingController _villageController = TextEditingController();
+  TextEditingController _hobliController = TextEditingController();
+  TextEditingController _villageController = TextEditingController();
 
   TextEditingController _village2Controller = TextEditingController();
   TextEditingController _synoController = TextEditingController();
@@ -49,116 +37,12 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   TextEditingController _panController = TextEditingController();
   TextEditingController _gstController = TextEditingController();
 
-  String districtDropdownValue = '';
-  String talukDropdownValue = '';
-  String hobliDropdownValue = '';
-  String villageDropdownValue = '';
+  final districts = addressList.districtTalukMap;
 
-  var districtList = [
-    '',
-    'District 1',
-    'District 2',
-    'District 3',
-    'District 4',
-    'District 5',
-    'District 6',
-    'District 7',
-    'District 8',
-  ];
+  List<String> taluks = [];
 
-  var talukList = [
-    '',
-    'Taluk A',
-    'Taluk B',
-    'Taluk C',
-    'Taluk D',
-    'Taluk E',
-    'Taluk F',
-    'Taluk G',
-    'Taluk H',
-  ];
-
-  var hobliList = [
-    '',
-    'Hobli X',
-    'Hobli Y',
-    'Hobli Z',
-    'Hobli P',
-    'Hobli Q',
-    'Hobli R',
-    'Hobli S',
-    'Hobli T',
-  ];
-
-  var villageList = [
-    '',
-    'Village 1',
-    'Village 2',
-    'Village 3',
-    'Village 4',
-    'Village 5',
-    'Village 6',
-    'Village 7',
-    'Village 8',
-  ];
-
-  void _viewDataFromHive() {
-    var dataBox = Hive.box<ProfileData>('profileDataBox3');
-
-    // dataBox.clear();
-    print('viewing them');
-
-    var savedData = dataBox.get('profile');
-
-    if (savedData != null) {
-      print('Length: ${savedData.address.length}');
-
-      for (var address in savedData.address) {
-        print(
-            'Address: ${address.district}, ${address.taluk}, ${address.hobli}, ${address.village}');
-      }
-    } else {
-      print('Data not found');
-    }
-  }
-
-  void _clearHive() {
-    var dataBox = Hive.box<ProfileData>('profileDataBox3');
-
-    // dataBox.clear();
-    print('Hive cleared');
-  }
-
-  // void _addAddressToHive(district, taluk, hobli, village) async {
-  //   var dataBox = Hive.box<ProfileData>('profileDataBox3');
-
-  //   var savedData = dataBox.get('profile');
-
-  //   var address = Address()
-  //     ..district = district
-  //     ..taluk = taluk
-  //     ..hobli = hobli
-  //     ..village = village;
-
-  //   if (savedData != null) {
-  //     print('object detected');
-  //     print(savedData.id);
-  //     var newData = ProfileData()
-  //       ..id = savedData.id
-  //       ..updated = savedData.updated
-  //       ..address = [...savedData.address, address]
-  //       ..kyc = savedData.kyc
-  //       ..land = savedData.land;
-
-  //     await dataBox.put(newData.id, newData);
-  //     print('object updated');
-  //   }
-
-  //   BlocProvider.of<HiveBloc>(context).add(
-  //     HiveDataRequested(),
-  //     // HiveAppendAddress(context: context),
-  //   );
-  // }
+  String districtDropdownValue = addressList.districtTalukMap[0][0] as String;
+  String talukDropdownValue = addressList.districtTalukMap[1][0] as String;
 
   void _addAddressToHive(district, taluk, hobli, village) async {
     if (district.isEmpty) {
@@ -172,12 +56,12 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       return;
     }
     if (hobli.isEmpty) {
-      ToastComponent.showDialog('Select Hobli',
+      ToastComponent.showDialog('Please Enter Hobli',
           gravity: Toast.center, duration: Toast.lengthLong);
       return;
     }
     if (village.isEmpty) {
-      ToastComponent.showDialog('Select Village',
+      ToastComponent.showDialog('Please Enter Village',
           gravity: Toast.center, duration: Toast.lengthLong);
       return;
     }
@@ -206,6 +90,9 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       print('object updated');
     }
 
+    _hobliController.clear();
+    _villageController.clear();
+
     BlocProvider.of<HiveBloc>(context).add(
       HiveDataRequested(),
       // HiveAppendAddress(context: context),
@@ -213,19 +100,22 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   }
 
   void _addLandToHive(area, syno, village) async {
-    if (area == 0) {
-      ToastComponent.showDialog('Enter Area name',
+    double areaDouble = double.parse(area);
+
+    if (village.isEmpty) {
+      ToastComponent.showDialog('Enter Village name',
           gravity: Toast.center, duration: Toast.lengthLong);
       return;
     }
+
     if (syno.isEmpty) {
       ToastComponent.showDialog('Enter Sy No',
           gravity: Toast.center, duration: Toast.lengthLong);
       return;
     }
 
-    if (village.isEmpty) {
-      ToastComponent.showDialog('Enter Village name',
+    if (area.isEmpty) {
+      ToastComponent.showDialog('Enter Area name',
           gravity: Toast.center, duration: Toast.lengthLong);
       return;
     }
@@ -235,7 +125,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     var savedData = dataBox.get('profile');
 
     var land = Land()
-      ..area = area
+      ..area = areaDouble
       ..syno = syno
       ..village = village
       ..crops = []
@@ -656,57 +546,57 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     SizedBox(
                       height: 10,
                     ),
-                    // TextFieldWidget(
-                    //     'District', _districtController, 'Enter District'),
+
+                    //Dropdown for district
                     DropdownButtonWidget(
-                        'District', districtList, districtDropdownValue,
-                        (value) {
+                        'District',
+                        districts.map((district) {
+                          return DropdownMenuItem<String>(
+                            value: district[0] as String,
+                            child: Text(district[0] as String),
+                          );
+                        }).toList(),
+                        districtDropdownValue, (value) {
                       setState(() {
                         districtDropdownValue = value;
+                        taluks = addressList.districtTalukMap.firstWhere(
+                                (element) =>
+                                    element[0] == districtDropdownValue)[1]
+                            as List<String>;
+                        talukDropdownValue = taluks[0];
                       });
                     }),
-                    DropdownButtonWidget('Taluk', talukList, talukDropdownValue,
-                        (value) {
+
+                    // Dropdown for Taluk
+                    DropdownButtonWidget(
+                        'Taluk',
+                        taluks.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        talukDropdownValue, (value) {
                       setState(() {
                         talukDropdownValue = value;
                       });
                     }),
-                    DropdownButtonWidget('Hobli', hobliList, hobliDropdownValue,
-                        (value) {
-                      setState(() {
-                        hobliDropdownValue = value;
-                      });
-                    }),
-                    DropdownButtonWidget(
-                        'Village', villageList, villageDropdownValue, (value) {
-                      setState(() {
-                        villageDropdownValue = value;
-                      });
-                    }),
+
+                    TextFieldWidget('Hobli', _hobliController, 'Enter Hobli'),
+                    TextFieldWidget(
+                        'Village', _villageController, 'Enter Village'),
+
                     Row(
                       children: [
                         Expanded(child: SizedBox()),
                         TextButton(
                           child: Text('Add Record'),
                           onPressed: () {
-                            // print('District: ${_districtController.text}');
-                            // print('taluk: ${_talukController.text}');
-                            // print('hobli: ${_hobliController.text}');
-                            // print('village: ${_villageController.text}');
-                            // _addAddressToHive(
-                            //     _districtController.text,
-                            //     _talukController.text,
-                            //     _hobliController.text,
-                            //     _villageController.text);
-                            // _districtController.clear();
-                            // _talukController.clear();
-                            // _hobliController.clear();
-                            // _villageController.clear();
                             _addAddressToHive(
                                 districtDropdownValue,
                                 talukDropdownValue,
-                                hobliDropdownValue,
-                                villageDropdownValue);
+                                _hobliController.text,
+                                _villageController.text);
                           },
                         ),
                       ],
@@ -777,8 +667,41 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     TextFieldWidget(
                         'Village', _village2Controller, 'Enter Village'),
                     TextFieldWidget('Syno', _synoController, 'Enter Syno'),
-                    TextFieldWidget(
-                        'Area', _areaController, 'Enter Area (in acres)'),
+
+                    //custom Area text field for accepting double values
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4, bottom: 5),
+                          child: Text(
+                            'Area',
+                            style: TextStyle(
+                                // color: MyTheme.accent_color,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: .5,
+                                fontFamily: 'Poppins'),
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          child: TextField(
+                            controller: _areaController,
+                            keyboardType:
+                                TextInputType.numberWithOptions(decimal: true),
+                            autofocus: false,
+                            decoration: InputDecorations.buildInputDecoration_1(
+                                hint_text: 'Enter Area (in acres)'),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        )
+                      ],
+                    ),
+
+                    //Add Land to Hive
                     Row(
                       children: [
                         Expanded(child: SizedBox()),
@@ -786,7 +709,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                           child: Text('Add Record'),
                           onPressed: () {
                             _addLandToHive(
-                              int.parse(_areaController.text).toDouble(),
+                              _areaController.text,
                               _synoController.text,
                               _village2Controller.text,
                             );
@@ -819,8 +742,11 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     );
   }
 
-  Column DropdownButtonWidget(String title, List<String> itemList,
-      String dropdownValue, Function(String) onChanged) {
+  Column DropdownButtonWidget(
+      String title,
+      List<DropdownMenuItem<String>>? itemList,
+      String dropdownValue,
+      Function(String) onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -860,12 +786,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               // This is called when the user selects an item.
               onChanged(value!);
             },
-            items: itemList.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
+            // items: itemList.map<DropdownMenuItem<String>>((String value) {
+            //   return DropdownMenuItem<String>(
+            //     value: value,
+            //     child: Text(value),
+            //   );
+            // }).toList(),
+            items: itemList,
           ),
         ),
         SizedBox(
