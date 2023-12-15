860089e07f640c35b0f26ab8c1b07bad5aeec191
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2023-12-15 22:00:32 +0530

    globaladdresses.dart file + location repo for it

--- lib/utils/location_repository.dart
+++ lib/utils/location_repository.dart
@@ -1,11 +1,14 @@
 import 'package:active_ecommerce_flutter/features/auth/services/auth_repository.dart';
 import 'package:active_ecommerce_flutter/features/auth/services/firestore_repository.dart';
+import 'package:active_ecommerce_flutter/utils/globaladdress.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LocationRepository {
   AuthRepository authRepository = AuthRepository();
   final FirestoreRepository firestoreRepository = FirestoreRepository();
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  List<Map> addresses = globaladdress;
 
   Future<List<String>> getDistrictsForPincode({
     required String pinCode,
@@ -30,82 +33,35 @@ class LocationRepository {
   }
 
   Future<List<String>> getAllDistricts() async {
-    try {
-      Set<String> districts = {};
-      final addressSnapshot =
-          await _firestore.collection('globaladdress').get();
-
-      addressSnapshot.docs.forEach((element) {
-        districts.add(element.data()['district']);
-      });
-
-      return districts.toList();
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to fetch locations. Please try again.');
-    }
+    return addresses.map((map) => map['district'].toString()).toSet().toList();
   }
 
   Future<List<String>> getTaluksForDistrict({
     required String districtName,
   }) async {
-    try {
-      Set<String> talukSet = {};
-      final addressSnapshot = await _firestore
-          .collection('globaladdress')
-          .where('district', isEqualTo: districtName)
-          .get();
-
-      addressSnapshot.docs.forEach((element) {
-        talukSet.add(element.data()['taluk']);
-      });
-
-      return talukSet.toList();
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to fetch locations. Please try again.');
-    }
+    return addresses
+        .where((map) => map['district'] == districtName)
+        .map((map) => map['TALUK'].toString())
+        .toSet()
+        .toList();
   }
 
   Future<List<String>> getGramPanchayatsForTaluk({
     required String taluk,
   }) async {
-    try {
-      Set<String> gramPanchayatSet = {};
-      final addressSnapshot = await _firestore
-          .collection('globaladdress')
-          .where('taluk', isEqualTo: taluk)
-          .get();
-
-      addressSnapshot.docs.forEach((element) {
-        gramPanchayatSet.add(element.data()['gramPanchayat']);
-      });
-
-      return gramPanchayatSet.toList();
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to fetch locations. Please try again.');
-    }
+    return addresses
+        .where((map) => map['TALUK'] == taluk)
+        .map((map) => map['Gram Panchayat'].toString())
+        .toSet()
+        .toList();
   }
 
   Future<List<String>> getVillagesForGramPanchayat({
     required String gramPanchayat,
   }) async {
-    try {
-      Set<String> villageNameSet = {};
-      final addressSnapshot = await _firestore
-          .collection('globaladdress')
-          .where('gramPanchayat', isEqualTo: gramPanchayat)
-          .get();
-
-      addressSnapshot.docs.forEach((element) {
-        villageNameSet.add(element.data()['villageName']);
-      });
-
-      return villageNameSet.toList();
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to fetch locations. Please try again.');
-    }
+    return addresses
+        .where((map) => map['Gram Panchayat'] == gramPanchayat)
+        .map((map) => map['Village Name'].toString())
+        .toList();
   }
 }
