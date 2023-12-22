e16c861b90e610344fca4a19db0617e71f6bd11a
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2023-12-22 21:46:36 +0530

    beginning of globalisations.

---
+++ lib/utils/globals.dart
@@ -0,0 +1,53 @@
+List<Map> products = [
+  {
+    'Cow': {
+      'name': {
+        'en': 'Cow',
+        'hi': 'गाय',
+        'kn': 'ಹಸು',
+      },
+      'image':
+          'https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg',
+      'categories': [
+        {'en': 'Black Cow', 'hi': 'काली गाय', 'kn': 'ಕಪ್ಪು ಹಸು'},
+        {'en': 'White Cow', 'hi': 'सफेद गाय', 'kn': 'ಬಿಳಿ ಹಸು'},
+        {'en': 'Brown Cow', 'hi': 'भूरी गाय', 'kn': 'ಕಂದು ಹಸು'},
+      ]
+    }
+  }
+];
+
+List<Map> products2 = [
+  {
+    'Cow': {
+      'name': 'Cow',
+      'image':
+          'https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg',
+      'categories': ['Black Cows', 'White Cows', 'Brown Cows']
+    }
+  }
+];
+
+var translations = [
+  {
+    'Cow': {
+      'en': 'Cow',
+      'hi': 'गाय',
+      'kn': 'ಹಸು',
+    },
+  },
+  {
+    'White Cow': {
+      'en': 'White Cow',
+      'hi': 'सफेद गाय',
+      'kn': 'ಬಿಳಿ ಹಸು',
+    },
+  },
+  {
+    'Brown Cow': {
+      'en': 'Brown Cow',
+      'hi': 'भूरी गाय',
+      'kn': 'ಕಂದು ಹಸು',
+    },
+  },
+];
